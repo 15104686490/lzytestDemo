86203fb5a1243ee2309f8df3a131e36543ce46de
lzy <455627872@qq.com>
2021-11-14 21:54:52 +0800

    java 1114

---
+++ src/main/java/demo/TestDemo.java
@@ -0,0 +1,15 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        String str = new String();
+        str.isEmpty();
+        ArrayList arrayList = new ArrayList();
+        arrayList.isEmpty();
+        //判断length size是否为零
+        AbstractQueuedSynchronizer abstractQueuedSynchronizer;
+    }
+}
